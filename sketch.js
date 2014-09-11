e75782017d094bad5428d7f0d9d5a449473ddcbf
Kimheejune <qwqw0102@hanmail.net>
2014-09-11 14:18:19 +0900

    1234

--- sketch.js
+++ sketch.js
@@ -2,10 +2,16 @@ function setup() {
   createCanvas(640, 480);
   // 0: black, 255: white
   // background(red, green, blue);
-  background(255, 0, 0);
-  stroke(255);
+  background(255, 50, 200);
+
   noFill();
+
+  stroke(255, 200, 180);
   line(10, 10, 90, 10);
-    rect(20, 20, 80, 50);
-    ellipse(50, 50, 50, 50);
+  
+  stroke(255, 180, 240);
+  rect(20, 20, 80, 50);
+
+  stroke(255, 241, 222);
+  ellipse(50, 50, 50, 50);
 }
